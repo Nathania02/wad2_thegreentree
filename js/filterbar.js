80c54d3f8fdd69d49a6a187b6ba58ffa9fc8d7aa
Nathania <nathaniayeo@gmail.com>
2023-10-27 17:41:20 +0800

    Updated filterbar.js

--- js/filterbar.js
+++ js/filterbar.js
@@ -1,21 +1,21 @@
 // open filter bar, ensure to name moving container "main"
-function openFilter(){
+function open_filter(){
     document.getElementById("filterbar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
   }
 
 // close filter bar, ensure to name moving container "main"
-function closeFilter(){
+function close_filter(){
     document.getElementById("filterbar").style.width = "0px";
     document.getElementById("main").style.marginLeft = "0px";
 }
 
-function createFilterBarCommunity(){
+function createfilterbar_community(){
     document.write(`
-    <button class="openbtn" onclick="openFilter()">Filters</button>
+    <button class="openbtn" onclick="open_filter()">Filters</button>
 
     <div id="filterbar">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeFilter()"><img id="back" src="images/back.png"></a>
+        <a href="javascript:void(0)" class="closebtn" onclick="close_filter()"><img id="back" src="images/back.png"></a>
         <a href="community_general.html">General</a>
         <a href="community_fashion.html">Fashion</a>
         <a href="community_food.html">Food</a>
